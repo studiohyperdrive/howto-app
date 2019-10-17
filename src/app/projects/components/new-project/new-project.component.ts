03f36dae323a26966a3218aed79fdeec1af82cea
Tom Opdebeeck <tom.opdebeeck@district01.be>
2019-10-18 01:01:38 +0200

    feat: new project dialog validation

--- src/app/projects/components/new-project/new-project.component.ts
+++ src/app/projects/components/new-project/new-project.component.ts
@@ -1,13 +1,19 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
+
+import { InstantErrorMatcher } from '../../../shared/utils/error-state-matcher';
 
 @Component({
 	selector: 'howto-dialog-new-project',
 	templateUrl: './new-project.component.html',
+	styleUrls: [
+		'./new-project.component.scss',
+	],
 })
 export class DialogNewProjectComponent implements OnInit {
 	public newProjectForm: FormGroup;
+	public errorStateMatcher = new InstantErrorMatcher();
 
 	constructor(
 		@Inject(MAT_DIALOG_DATA) public data: any,
@@ -16,7 +22,10 @@ export class DialogNewProjectComponent implements OnInit {
 
 	public ngOnInit(): void {
 		this.newProjectForm = new FormGroup({
-			name: new FormControl('', [Validators.required]),
+			name: new FormControl('', [
+				Validators.required,
+				this.projectDoesNotExist(),
+			]),
 		});
 	}
 
@@ -26,5 +35,19 @@ export class DialogNewProjectComponent implements OnInit {
 
 	public handleFormSubmit(): void {
 		this.dialogRef.close(this.newProjectForm.value.name);
+	}
+
+	private projectDoesNotExist(): ValidatorFn {
+		return (control: FormControl): ValidationErrors => {
+			if (control.pristine || !control.value) {
+				return null;
+			}
+
+			const projects = this.data.projects || [];
+
+			return projects.includes(control.value) ? {
+				projectExists: true,
+			} : null;
+		};
 	}
 }
