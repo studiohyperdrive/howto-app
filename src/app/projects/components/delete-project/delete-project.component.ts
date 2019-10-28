23235bdbda0c442ab01c36618b1ba3dd4da16573
Tom Opdebeeck <tom.opdebeeck@district01.be>
2019-10-28 21:47:11 +0100

    fix: indenting

--- src/app/projects/components/delete-project/delete-project.component.ts
+++ src/app/projects/components/delete-project/delete-project.component.ts
@@ -2,22 +2,22 @@ import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-	selector: 'howto-dialog-delete-project',
-	templateUrl: './delete-project.component.html',
-	styleUrls: [
-		'./delete-project.component.scss',
-	],
+  selector: 'howto-dialog-delete-project',
+  templateUrl: './delete-project.component.html',
+  styleUrls: [
+    './delete-project.component.scss',
+  ],
 })
 export class DialogDeleteProjectComponent {
-	constructor(
-		public dialogRef: MatDialogRef<DialogDeleteProjectComponent>,
-	) {}
+  constructor(
+    public dialogRef: MatDialogRef<DialogDeleteProjectComponent>,
+  ) { }
 
-	public onCancel(): void {
-		this.dialogRef.close();
-	}
+  public onCancel(): void {
+    this.dialogRef.close();
+  }
 
-	public onDelete(): void {
-		this.dialogRef.close(true);
-	}
+  public onDelete(): void {
+    this.dialogRef.close(true);
+  }
 }
--- src/app/projects/components/project-card/project-card.component.ts
+++ src/app/projects/components/project-card/project-card.component.ts
@@ -3,10 +3,10 @@ import { Component, Input } from '@angular/core';
 import { Project } from '../../types/project';
 
 @Component({
-	selector: 'howto-project-card',
-	templateUrl: './project-card.component.html',
-	styleUrls: [ './project-card.component.css' ],
+  selector: 'howto-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.css'],
 })
 export class ProjectCardComponent {
-	@Input() public project: Project;
+  @Input() public project: Project;
 }
